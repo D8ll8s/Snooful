022790c1a6101f0576227e57b16d7f128f8ad061
D8ll8s <65066084+D8ll8s@users.noreply.github.com>
2020-05-09 05:59:51 -0400

    Create yomama.js

---
+++ src/commands/yomama.js
@@ -0,0 +1,10 @@
+
+
+const url = require("url-escape-tag");
+// const program = require("commander");
+const request = require("request");
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
+var pfp_url = ""
+
+args.send("hello");
